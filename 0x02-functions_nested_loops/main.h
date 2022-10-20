153755955b6871cdda0538579754a2a2f25fe5d1
Olumide Rotimi <rotimiolumide68@gmail.com>
2022-10-19 22:50:55 -0700

    Print alphabet in lower case

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef Main_H
 #define Main_H
 /*declare _putchar*/
-int _putchar(char c); 
+int _putchar(char c);
+void print_alphabet(void);                                                                            
 #endif /*Main_H*/
