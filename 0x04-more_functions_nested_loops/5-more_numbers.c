31b01ce5d3a374ea71e32641de4e12b91097edae
Olumide Rotimi <rotimiolumide68@gmail.com>
2022-10-21 06:13:29 -0700

    Print integers from 0 t0 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers 0 to 14 ten times
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i <= 10)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar( (j % 10) + '0');
+		}
+		i++;
+		_putchar('\n');
+	}
+}
