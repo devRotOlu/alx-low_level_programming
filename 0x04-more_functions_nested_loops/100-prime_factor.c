fecc6ced01e7334a71e88505d6075a27e887e47a
Olumide Rotimi <rotimiolumide68@gmail.com>
2022-10-21 23:13:13 -0700

    Print prime numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,20 +6,21 @@
 /**
  * main - Prints the largest prime factor of 612852475143
  *
- * Return : Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	long x, maxf;
 
 	long number = 612852475143;
-
-	double square = sqrt(number);
+	double square =  sqrt(number);
 
 	for (x = 1; x <= square; x++)
 	{
 		if (number % x == 0)
+		{
 			maxf = number / x;
+		}
 	}
 
 	printf("%ld\n", maxf);
