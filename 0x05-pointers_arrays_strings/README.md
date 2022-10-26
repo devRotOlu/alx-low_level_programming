b6a0bb88c4be44693da21ad4bdb4e02915d4a00a
Olumide Rotimi <rotimiolumide68@gmail.com>
2022-10-25 21:29:44 -0700

    Update the value of variable pointed to

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Working with arrays, strings, and pointers
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef Main_H
+#define Main_H
+/*declare _putchar*/
+int _putchar(char c);
+void print_alphabet(void);  
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);  
+void reset_to_98(int *n);
+#endif /*Main_H*/
