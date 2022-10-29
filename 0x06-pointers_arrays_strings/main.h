df970967e90f0d0c767c3201d0f7d25876f8591f
Olumide Rotimi <rotimiolumide68@gmail.com>
2022-10-28 18:09:31 -0700

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef Main_H
+#define Main_H
+/*declare _putchar*/
+int _putchar(char c);
+void print_alphabet(void);  
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);  
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(const char *str);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+#endif /*Main_H*/
